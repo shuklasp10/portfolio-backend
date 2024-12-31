3d318990bf7b7a36007289a1db522f1656d1a74f
Shri Prakash Shukla <44600381+shuklasp10@users.noreply.github.com>
2024-12-31 07:28:12 +0000

    add css and js

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const {getAdmin} = require('../controllers/adminController');
+const path = require('path')
 
 const router = express.Router()
+
+router.use('/',express.static(path.join(__dirname,'..','public')));
 
 router.get('/',getAdmin)
 
